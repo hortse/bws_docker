f9f795ba2ca6499b2b7ac1175a6ddbb8825e78de
hortse <senchenko.sergey@gmail.com>
2020-04-24 17:13:53 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y remove docker docker-engine docker.io
+sudo apt-get -y install docker.io
 sudo systemctl enable docker
-sudo apt-get -y install python-pip
+sudo systemctl start docker
+sudo apt-get -y install python3-pip
 sudo pip install pyopenssl docker-compose
 sudo mkdir -p /opt/wordpress
 cd /opt/wordpress
