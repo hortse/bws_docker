ed55bc5362b5b2072fb082eedf3d51522ded27b6
hortse <senchenko.sergey@gmail.com>
2020-04-28 18:20:00 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,49 +1,64 @@
 #!/bin/bash
 #Docker Wordpress environment v0.1
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run script with sudo"
+  exit
+fi
+
 STARTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-if [ ! $1 ] || [ $1 == "install" ] || [ $1 == "remove" ] || [ $1 == "start" ] || [ $1 == "stop" ] || [ $1 == "info" ] || [ $1 = "uninstall" ] || [ $1 = "help" ]
+if ! [[ $# -ne 0 ]]
 then
-        echo "Usage : install | remove | start | stop | info | uninstall | help"
+echo "Usage : install | remove | start | stop | info | uninstall | help"
         exit
 fi
 
-case $1 in 
+if  [[ $1 = "install" ]] ||  [[ $1 = "remove" ]] ||  [[ $1 = "start" ]] ||  [[ $1 = "stop" ]] || [[ $1 = "info" ]] || [[ $1 = "uninstall" ]] || [[ $1 = "help" ]] 
+then
+
+case "$1" in 
      install)
      
-INST_ERR = false
 sudo apt-get -y update && \
-sudo apt-get -y remove docker docker-engine docker.io && \
+sudo apt-get -y remove docker.io && \
 sudo apt-get -y install docker.io && \
+sleep 3 && \
 sudo systemctl enable docker && \
 sudo systemctl start docker && \
+sleep 3 && \
 sudo apt-get -y install python3-pip && \
+sleep 3 && \
 sudo pip3 install pyopenssl docker-compose && \
-sudo $STARTDIR/docker-compose up -d || INST_ERR = true
+sleep 3 && \
+cd "$STARTDIR" && \
+sudo docker-compose up -d 
 
-if [ $INST_ERR ]
+if [[ $(sudo docker ps -q | wc -l) != 3 ]]
 then
 echo "Errors occurred during installation"
 exit
 else
+echo " "
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Installation was successful"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo " "
 echo "In order to connect website use following ports:"
 sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  wordpress-site
 echo "-----------------"
 echo "In order to connect phpmyadmin use following ports:"
-sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  wordpress_phpmyadmin_1
+sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  bws_docker_phpmyadmin_1
 echo "-----------------"
-echo "In order to connect database use following ports:"
-sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  wordpress_db_1
 fi
 ;;
 
-case $1 in 
+
      remove)
 
 echo "All Docker containers will be removed!"
 echo "y|N"
-read ANS
-if [ $ANS == "y" || $ANS == "Y" ]
+read -r ANS
+if [[ $ANS = "y" ]] || [[ $ANS = "Y" ]]
 then 
 sudo docker container stop $(sudo docker container ls -aq)
 sudo docker container rm $(sudo docker container ls -aq)
@@ -53,36 +68,38 @@ exit
 fi
 ;;
 
-case $1 in 
+
      start)
      
-START_ERR = false
-sudo $STARTDIR/docker-compose up -d || START_ERR = true
+cd "$STARTDIR"
+sudo docker-compose up -d
 
-if [ $INST_ERR ]
+if [[ $(sudo docker ps -q | wc -l) != 3 ]]
 then
 echo "Errors occurred during start up. Check if environment is installed"
 exit
 else
+echo " "
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "Start up was successful"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo " "
 echo "In order to connect website use following ports:"
 sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  wordpress-site
 echo "-----------------"
 echo "In order to connect phpmyadmin use following ports:"
-sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  wordpress_phpmyadmin_1
+sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  bws_docker_phpmyadmin_1
 echo "-----------------"
-echo "In order to connect database use following ports:"
-sudo docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}} -> {{(index $conf 0).HostPort}} {{end}}'  wordpress_db_1
 fi
 ;;
 
-case $1 in 
+ 
      stop)
      
 sudo docker container stop $(sudo docker container ls -aq)
 ;;
 
-case $1 in 
+
      info)
 docker -v
 echo "------------------" 
@@ -91,14 +108,14 @@ echo "------------------"
 sudo docker ps
 ;;
 
-case $1 in 
+
      uninstall)
      
 echo "The following packages will be removed:"
 echo "docker.io, python3-pip"
 echo "y|N"
-read ANS
-if [ $ANS == "y" || $ANS == "Y" ]
+read -r ANS
+if [[ $ANS = "y" ]] || [[ $ANS = "Y" ]]
 then 
 
 sudo docker container stop $(sudo docker container ls -aq)
@@ -113,7 +130,7 @@ exit
 fi
 ;;
 
-case $1 in 
+ 
      help)
      
 echo "Docker Wordpress developer's environment script"
@@ -125,3 +142,8 @@ echo "run.sh stop - Shut down all containers"
 echo "run.sh info - Information about installed components and running containers"
 ;;
 esac
+
+else
+echo "Usage : install | remove | start | stop | info | uninstall | help"
+        exit
+fi
